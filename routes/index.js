100016b8bdc711c5c11270b744d96450678f5b83
ihascats <s.pavlovic.rs@gmail.com>
2022-09-03 14:47:19 +0200

    Add a post route path for the form

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,46 @@
 const express = require('express');
 const router = express.Router();
+const Item = require('../models/Item');
+
+const multer = require('multer');
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 router.get('/', function (req, res, next) {
   res.render('index');
+});
+
+router.post('/new-item-create', upload.single('image'), function (req, res) {
+  console.log(req.file);
+  if (req.file) {
+    const newItem = new Item({
+      name: req.body.name,
+      description: req.body.description,
+      category: req.body.category,
+      price: req.body.price,
+      number_in_stock: req.body.number_in_stock,
+      image: req.file.filename,
+    });
+    newItem.save();
+  } else {
+    const newItem = new Item({
+      name: req.body.name,
+      description: req.body.description,
+      category: req.body.category,
+      price: req.body.price,
+      number_in_stock: req.body.number_in_stock,
+    });
+    newItem.save();
+  }
+  res.redirect('/');
 });
 
 module.exports = router;
