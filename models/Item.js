27b262f549c09c482174412586d200f0cd9aa25d
ihascats <s.pavlovic.rs@gmail.com>
2022-09-03 14:11:25 +0200

    Create schema

---
+++ models/Item.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const ItemSchema = new mongoose.Schema({
+  name: { type: String, required: true, minLength: 2, maxLength: 64 },
+  description: { type: String, required: true, minLength: 8, maxLength: 256 },
+  category: [
+    { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },
+  ],
+  price: { type: Number, required: true },
+  number_in_stock: { type: Number, required: true },
+  image: { type: String, default: 'no-image.png' },
+});
+
+ItemSchema.virtual('url').get(function () {
+  return '/store/' + this._id;
+});
+
+module.exports = mongoose.model('Items', ItemSchema);
