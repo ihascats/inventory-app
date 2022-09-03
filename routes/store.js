41428e2fbab6c4e842a2ce3442e150c568830f3a
ihascats <s.pavlovic.rs@gmail.com>
2022-09-03 17:00:52 +0200

    Add sort by category to the store

--- routes/store.js
+++ routes/store.js
@@ -6,4 +6,10 @@ router.get('/', async function (req, res, next) {
   res.render('store', { items: await Item.find() });
 });
 
+router.get('/sortBy-:category', async function (req, res, next) {
+  res.render('store', {
+    items: await Item.find({ category: req.params.category }),
+  });
+});
+
 module.exports = router;
